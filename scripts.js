09340ac55aa52c4af0dd46738c7347d4ad426067
AnarRussia1219 <ft2j5fqf@duck.com>
2024-06-29 03:02:56 +0100

    cretae function for rendering books

--- scripts.js
+++ scripts.js
@@ -14,3 +14,33 @@ function Book(title, author, pages, read) {
 Book.prototype.toggleRead = function() {
     this.read = !this.read;
 };
+
+function renderBooks() {
+    document.querySelector(".user-books-grid").textContent = "";
+
+    myLibrary.forEach(book => {
+        const newBookCard = document.createElement("div");
+        newBookCard.classList.add("user-book-card");
+        newBookCard.innerHTML = `
+            <div class="user-book-card-data">
+                <p class="user-book-card-book-title">${book.title}</p>
+                <p class="user-book-card-book-author">${book.author}</p>
+                <p class="user-book-card-book-pages">${book.pages}</p>
+            </div>
+            ${function() {
+                if (book.read) {
+                    return `<button class="user-book-card-toggle-read-btn">Read</button>`;
+                } else {
+                    return `<button class="user-book-card-toggle-read-btn unread">Unread</button>`;
+                };
+            }()}
+        `;
+        document.querySelector(".user-books-grid").appendChild(newBookCard);
+    });
+};
+
+renderBooks();
+
+document.getElementById("page-header-add-book-btn").addEventListener("click", function() {
+    
+});
